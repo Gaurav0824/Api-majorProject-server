6721e436d144bb59cbb816084317b1b5a3935b77
Gaurav Kumar Gupta <gauravgupta1999kr@gmail.com>
2022-06-20 23:56:19 +0530

    update

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def processed_data(text, stop_remove, more_remove, stem, lemme):
     from nltk.stem import WordNetLemmatizer
     lemmatizer = WordNetLemmatizer()
     text = [lemmatizer.lemmatize(w) for w in text]
-    text = (" ".join(text))  # joining of the text .
+    text = ("".join(text))  # joining of the text .
     print(text)
     return text
 
